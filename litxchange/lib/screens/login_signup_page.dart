db909780d37e6136d497b2b3bf41b4fd79d102af
ridaamirr <ridaamirbashir@gmail.com>
2024-03-02 17:30:11 +0500

    signup frontend

--- litxchange/lib/screens/login_signup_page.dart
+++ litxchange/lib/screens/login_signup_page.dart
@@ -45,7 +45,7 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
+ /* Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Sign Up')),
       body: Padding(
@@ -79,6 +79,167 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
           ),
         ),
       ),
+    );
+  }*/
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/background.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 30,
+                        width: 80,
+                        height: 200,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/light-1.png')
+                              )
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 140,
+                        width: 80,
+                        height: 150,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/light-2.png')
+                              )
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 40,
+                        top: 40,
+                        width: 80,
+                        height: 150,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/clock.png')
+                              )
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 50),
+                          child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Color.fromRGBO(143, 148, 251, 1)),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, .2),
+                                  blurRadius: 20.0,
+                                  offset: Offset(0, 10)
+                              )
+                            ]
+                        ),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color:  Color.fromRGBO(143, 148, 251, 1)))
+                                ),
+                                child: TextFormField(
+                                  controller: _emailController,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    labelText: 'Email',
+                                  ),
+                                  validator: (value) =>
+                                  value!.isEmpty ? 'Please enter an email' : null,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                child: TextFormField(
+                                  controller: _passwordController,
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    labelText: 'Password',
+                                  ),
+                                  validator: (value) => value!.length < 6
+                                      ? 'Password must be at least 6 characters'
+                                      : null,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                child: TextFormField(
+                                  controller: _confirmPasswordController,
+                                  obscureText: true,
+                                  decoration:
+                                  InputDecoration(labelText: 'Confirm Password'),
+                                  validator: (value) =>
+                                  value != _passwordController.text
+                                      ? 'Passwords do not match'
+                                      : null,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30,),
+                      Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                colors: [
+                                  Color.fromRGBO(143, 148, 251, 1),
+                                  Color.fromRGBO(143, 148, 251, .6),
+                                ]
+                            )
+                        ),
+                        child: ElevatedButton(
+                          onPressed: _registerUser,
+                          child: const Text('Sign Up'),
+                        ),
+                      ),
+                      SizedBox(height: 70,),
+                      Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
     );
   }
 }
