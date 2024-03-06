88d051b10445801c6178def31e00a77e966cafda
ridaamirr <ridaamirbashir@gmail.com>
2024-03-06 22:32:38 +0500

    displaying posts on homepage
    only printing rn

--- litxchange/lib/screens/home.dart
+++ litxchange/lib/screens/home.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return Scaffold(
+      body: PostList(),
+    );
+  }
+}
+
+class PostList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final currentUserUid = FirebaseAuth.instance.currentUser!.uid;
+
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('posts')
+          .where('userId', isNotEqualTo: currentUserUid)
+          .snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+        final posts = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            var post = posts[index];
+            return ListTile(
+              title: Text(post['bookName']),
+              subtitle: Text(post['description']),
+              trailing: post['imageUrl'] != null
+                  ? Image.network(
+                post['imageUrl'],
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              )
+                  : null,
+            );
+          },
+        );
+      },
     );
   }
 }
