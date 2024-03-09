d9c754f1431de2009a9fed7e3b7893ffdac6adc8
haisemishere <91388126+haisemishere@users.noreply.github.com>
2024-03-09 12:26:48 +0500

    add post front end fixed

--- litxchange/lib/screens/add.dart
+++ litxchange/lib/screens/add.dart
@@ -32,9 +32,64 @@ class AddPage extends StatefulWidget {
 
 class _AddPageState extends State<AddPage> {
   final TextEditingController _bookNameController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  final TextEditingController _genreController = TextEditingController();
+  String _selectedCondition = 'New'; // Default selected condition
+  String _selectedGenre = 'Fiction'; // Default selected genre
   File? _image;
+
+  final List<String> _conditionItems = ['New', 'Like New', 'Very Good', 'Good', 'Acceptable'];
+
+  final List<String> _genreItems = [
+    'Fiction',
+    'Non-fiction',
+    'Mystery',
+    'Thriller',
+    'Science Fiction (Sci-Fi)',
+    'Fantasy',
+    'Romance',
+    'Historical Fiction',
+    'Horror',
+    'Adventure',
+    'Crime',
+    'Biography',
+    'Autobiography',
+    'Memoir',
+    'Young Adult (YA)',
+    "Children's",
+    'Comedy',
+    'Satire',
+    'Poetry',
+    'Drama',
+    'Self-help',
+    'Business',
+    'Travel',
+    'Science',
+    'Philosophy',
+    'Psychology',
+    'Religion/Spirituality',
+    'Cookbooks',
+    'Art/Photography',
+    'Graphic Novels/Comics',
+    'Music',
+    'Sports',
+    'Health/Fitness',
+    'Education',
+    'Technology',
+    'Parenting/Family',
+    'LGBTQ+',
+    'Military/War',
+    'Supernatural/Paranormal',
+    'Western',
+    'Urban',
+    'Environmental',
+    'Dystopian',
+    'Utopian',
+    'Post-Apocalyptic',
+    'Steampunk',
+    'Cyberpunk',
+    'Gothic',
+    'Bildungsroman (Coming-of-age)',
+    'Experimental/Avant-Garde',
+  ];
 
   Future<void> _getImage() async {
     final picker = ImagePicker();
@@ -54,101 +109,119 @@ class _AddPageState extends State<AddPage> {
       appBar: AppBar(
         title: Text('New Post'),
       ),
-      body: SingleChildScrollView( // Make the content scrollable
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Book Name',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _bookNameController,
-              decoration: InputDecoration(
-                hintText: 'Enter the book name',
+      body: SingleChildScrollView( // Wrap the entire content with SingleChildScrollView
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Book Name',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Description',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _descriptionController,
-              maxLines: 3,
-              decoration: InputDecoration(
-                hintText: 'Enter a description',
+              SizedBox(height: 8),
+              TextFormField(
+                controller: _bookNameController,
+                decoration: InputDecoration(
+                  hintText: 'Enter the book name',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Genre',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _genreController,
-              decoration: InputDecoration(
-                hintText: 'Enter the genre',
+              SizedBox(height: 16),
+              Text(
+                'Book Condition',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _getImage,
-              child: Text('Pick Image'),
-            ),
-            SizedBox(height: 20),
-            _image != null
-                ? Container(
-              // Constrain the size of the image
-              height: 200, // Set a fixed height for the image
-              child: Image.file(_image!, fit: BoxFit.cover), // Adjust the fit as needed
-            )
-                : SizedBox(height: 2, width: 2),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Validate and save the form data
-                _savePost();
-              },
-              child: Text('Post'),
-            ),
-          ],
+              SizedBox(height: 8),
+              DropdownButtonFormField<String>(
+                value: _selectedCondition,
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedCondition = newValue!;
+                  });
+                },
+                items: _conditionItems.map((condition) {
+                  return DropdownMenuItem<String>(
+                    value: condition,
+                    child: Text(condition),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Genre',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              DropdownButtonFormField<String>(
+                value: _selectedGenre,
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedGenre = newValue!;
+                  });
+                },
+                items: _genreItems.map((genre) {
+                  return DropdownMenuItem<String>(
+                    value: genre,
+                    child: Text(genre),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _getImage,
+                child: Text('Pick Image'),
+              ),
+              SizedBox(height: 16),
+              _image != null
+                  ? Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: FileImage(_image!),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+                  : SizedBox.shrink(),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  _savePost();
+                },
+                child: Text('Post'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
-  void _savePost() async
-  {
+  void _savePost() async {
     String bookName = _bookNameController.text.trim();
-    String description = _descriptionController.text.trim();
-    String genre = _genreController.text.trim();
-
-    // Initialize imageUrl as null.
+    String condition = _selectedCondition;
+    String genre = _selectedGenre;
     String? imageUrl;
 
     try {
       if (_image != null) {
         final storageRef = FirebaseStorage.instance.ref().child('images/${DateTime.now()}.png');
         final uploadTask = storageRef.putFile(_image!);
-
-        // Await for the upload to complete and then get the download URL.
         await uploadTask;
         imageUrl = await storageRef.getDownloadURL();
       }
 
-      // Proceed to save the post with or without an imageUrl.
       await FirebaseFirestore.instance.collection('posts').add({
         'userId': FirebaseAuth.instance.currentUser!.uid,
         'bookName': bookName,
-        'description': description,
+        'condition': condition,
         'genre': genre,
         'date': DateTime.now(),
-        'imageUrl': imageUrl ?? "", // Use the imageUrl if available; otherwise, use an empty string.
+        'imageUrl': imageUrl ?? '',
       });
 
-      // Show a success dialog
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -158,10 +231,10 @@ class _AddPageState extends State<AddPage> {
             TextButton(
               onPressed: () {
                 _bookNameController.clear();
-                _descriptionController.clear();
-                _genreController.clear();
                 setState(() {
-                  _image = null; // Reset the image
+                  _selectedCondition = _conditionItems[0];
+                  _selectedGenre = _genreItems[0];
+                  _image = null;
                 });
                 Navigator.of(context).pop();
               },
@@ -171,7 +244,6 @@ class _AddPageState extends State<AddPage> {
         ),
       );
     } catch (error) {
-      // Show an error dialog if anything goes wrong during the upload or Firestore save process.
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -189,47 +261,7 @@ class _AddPageState extends State<AddPage> {
       );
       print("Error adding post: $error");
     }
-    // Assuming you have a Post model class
-    // This part of the code depends on how you've set up Firestore data model
-    // and might require adjustments based on your Firestore collection structure
-    FirebaseFirestore.instance.collection('posts').add({
-      'userId': FirebaseAuth.instance.currentUser!.uid,
-      'bookName': bookName,
-      'description': description,
-      'genre': genre,
-      'date': DateTime.now(),
-      'imageUrl': imageUrl, // Store the image URL in Firestore
-    }).then((value) => showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Post Successful'),
-        content: Text('Post Added successfully'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              _bookNameController.clear();
-              _descriptionController.clear();
-              _genreController.clear();
-              Navigator.of(context).pop();
-            },
-            child: Text('OK'),
-          ),
-        ],
-      ),
-    )).catchError((error) => showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Post Failed'),
-        content: Text('Unable to add this post'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('OK'),
-          ),
-        ],
-      ),
-    ));
   }
 }
+
+
