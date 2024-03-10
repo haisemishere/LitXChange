c4ec977faf8a08543a9f864859d87e857985fea2
Excaliodus <excaliodus@gmail.com>
2024-03-10 18:25:09 +0500

    V 0.7
    Profile Pic Updating

--- litxchange/lib/screens/profile.dart
+++ litxchange/lib/screens/profile.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:intl/intl.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:litxchange/screens/login.dart';
 
 void main() async {
@@ -17,6 +19,7 @@ class ProfilePage extends StatefulWidget {
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
+
 class _ProfilePageState extends State<ProfilePage> {
   late FirebaseFirestore _firestore;
   late String _userId;
@@ -31,33 +34,18 @@ class _ProfilePageState extends State<ProfilePage> {
     super.initState();
     _firestore = FirebaseFirestore.instance;
     _fetchUserData();
-    _fetchUserEmail(); // Fetch user email
   }
 
   Future<void> _fetchUserData() async {
-    try {
-      User? user = FirebaseAuth.instance.currentUser;
-      if (user != null) {
-        _userId = user.uid;
-        DocumentSnapshot userData =
-        await _firestore.collection('users').doc(_userId).get();
-        setState(() {
-          _username = userData['userName'] ?? '';
-          _bio = userData['bio'] ?? '';
-          _city = userData['city'] ?? '';
-          _profilePictureUrl =
-              userData['profilePictureUrl'] ?? "https://via.placeholder.com/150";
-        });
-      }
-    } catch (error) {
-      print("Error fetching user data: $error");
-    }
-  }
-
-  Future<void> _fetchUserEmail() async {
-    final user = FirebaseAuth.instance.currentUser;
+    User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
+      _userId = user.uid;
+      var userData = await _firestore.collection('users').doc(_userId).get();
       setState(() {
+        _username = userData.data()?['userName'] ?? '';
+        _bio = userData.data()?['bio'] ?? '';
+        _city = userData.data()?['city'] ?? '';
+        _profilePictureUrl = userData.data()?['profilePictureUrl'] ?? "https://via.placeholder.com/150";
         _userEmail = user.email ?? '';
       });
     }
@@ -67,279 +55,120 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         title: Text('Profile'),
         actions: [
           IconButton(
             icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditProfilePage(
-                    currentUsername: _username,
-                    currentBio: _bio,
-                    currentCity: _city,
-                    onUpdate: _updateUserData,
-                    userEmail: _userEmail, // Pass user email to EditProfilePage
-                  ),
-                ),
-              );
-            },
+            onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfilePage(
+              currentUsername: _username,
+              currentBio: _bio,
+              currentCity: _city,
+              currentEmail: _userEmail,
+              profilePictureUrl: _profilePictureUrl,
+              onUpdate: (newUsername, newBio, newCity, newProfilePictureUrl) {
+                setState(() {
+                  _username = newUsername;
+                  _bio = newBio;
+                  _city = newCity;
+                  _profilePictureUrl = newProfilePictureUrl;
+                });
+                _saveUserData();
+              },
+            ))),
           ),
           IconButton(
             icon: Icon(Icons.logout),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Login(),
-                ),
-              );
-            },
+            onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login())),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 20),
-            Center(
-              child: CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage(_profilePictureUrl),
-                backgroundColor: Colors.transparent,
-              ),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(_profilePictureUrl),
             ),
             SizedBox(height: 20),
-            Center(
-              child: Text(
-                _username,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
+            Text(_username, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             SizedBox(height: 10),
-            Center(
-              child: Text(
-                _bio,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
+            Text(_bio, style: TextStyle(fontSize: 16)),
             SizedBox(height: 10),
-            Center(
-              child: Text(
-                _city,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Divider(), // Add a divider to separate profile info from posts
-            SizedBox(height: 10),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-            ),
-            SizedBox(height: 10),
-            _buildUserPostsList(),
+            Text(_city, style: TextStyle(fontSize: 16)),
+            // Add more widgets here for user posts and other info
           ],
         ),
       ),
     );
   }
 
-  Widget _buildUserPostsList() {
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance
-          .collection('posts')
-          .where('userId', isEqualTo: _userId) // Filter posts by current user's ID
-          .snapshots(),
-      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        if (snapshot.hasError) {
-          return Center(
-            child: Text('Error: ${snapshot.error}'),
-          );
-        }
-        final posts = snapshot.data!.docs;
-        if (posts.isEmpty) {
-          return Center(
-            child: Text('No posts available'),
-          );
-        }
-        return ListView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          itemCount: posts.length,
-          itemBuilder: (context, index) {
-            var post = posts[index];
-            var date = post['date'].toDate();
-            var formattedDate = DateFormat.yMMMMd().format(date);
-            String authorName = post['authorName'] ?? 'Unknown Author';
-            return Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                    ),
-                    ListTile(
-                      title: Text(
-                        '${post['bookName']} by $authorName',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(post['genre']),
-                    ),
-                    post['imageUrl'] != null
-                        ? Image.network(
-                      post['imageUrl'],
-                      fit: BoxFit.cover,
-                    )
-                        : SizedBox.shrink(),
-                    SizedBox(height: 8.0),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            formattedDate,
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: Text("Delete Post"),
-                                    content: Text(
-                                        "Are you sure you want to delete this post?"),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.pop(
-                                              context); // Close the dialog
-                                        },
-                                        child: Text("Cancel"),
-                                      ),
-                                      TextButton(
-                                        onPressed: () async {
-                                          // Delete the post from Firestore
-                                          await FirebaseFirestore.instance
-                                              .collection('posts')
-                                              .doc(post.id) // Use the document's ID
-                                              .delete();
-                                          Navigator.pop(
-                                              context); // Close the dialog
-                                        },
-                                        child: Text("Delete"),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  void _updateUserData(String newUsername, String newBio, String newCity) {
-    setState(() {
-      _username = newUsername;
-      _bio = newBio;
-      _city = newCity;
-    });
-    _saveUserData();
-  }
-
   void _saveUserData() async {
-    try {
-      await _firestore.collection('users').doc(_userId).set({
-        'username': _username,
-        'bio': _bio,
-        'city': _city,
-      });
-      print('User data saved successfully');
-    } catch (error) {
-      print('Error saving user data: $error');
-    }
+    await _firestore.collection('users').doc(_userId).update({
+      'userName': _username,
+      'bio': _bio,
+      'city': _city,
+      'profilePictureUrl': _profilePictureUrl,
+    });
   }
 }
 
+// Modify the EditProfilePage to include the functionality for updating profile pictures
 class EditProfilePage extends StatefulWidget {
   final String currentUsername;
   final String currentBio;
   final String currentCity;
-  final Function(String, String, String) onUpdate;
-  final String userEmail; // User email
+  final String currentEmail;
+  final String profilePictureUrl;
+  final Function(String, String, String, String) onUpdate;
 
-  EditProfilePage({
+  const EditProfilePage({
+    Key? key,
     required this.currentUsername,
     required this.currentBio,
     required this.currentCity,
+    required this.currentEmail,
+    required this.profilePictureUrl,
     required this.onUpdate,
-    required this.userEmail, // Pass user email to EditProfilePage
-  });
+  }) : super(key: key);
 
   @override
   _EditProfilePageState createState() => _EditProfilePageState();
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-  late TextEditingController _usernameController;
-  late TextEditingController _bioController;
-  late TextEditingController _cityController;
-  String _userEmail = '';
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  File? _profileImage;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
-    _fetchUserEmail();
     super.initState();
-    _usernameController =
-        TextEditingController(text: widget.currentUsername);
-    _bioController = TextEditingController(text: widget.currentBio);
-    _cityController = TextEditingController(text: widget.currentCity);
+    _usernameController.text = widget.currentUsername;
+    _bioController.text = widget.currentBio;
+    _cityController.text = widget.currentCity;
   }
 
-  Future<void> _fetchUserEmail() async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
       setState(() {
-        _userEmail = user.email ?? '';
+        _profileImage = File(pickedFile.path);
       });
+    }
+  }
+
+  Future<void> _uploadImage() async {
+    if (_profileImage != null) {
+      String fileName = 'profile_${widget.currentUsername}_${DateTime.now().millisecondsSinceEpoch}.jpg';
+      final firebaseStorageRef = FirebaseStorage.instance.ref().child('profile_images/$fileName');
+      await firebaseStorageRef.putFile(_profileImage!);
+      final downloadUrl = await firebaseStorageRef.getDownloadURL();
+      widget.onUpdate(_usernameController.text, _bioController.text, _cityController.text, downloadUrl);
+    } else {
+      widget.onUpdate(_usernameController.text, _bioController.text, _cityController.text, widget.profilePictureUrl);
     }
   }
 
@@ -347,71 +176,39 @@ class _EditProfilePageState extends State<EditProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-        title: Text('Edit Profile'),
+        title: Text("Edit Profile"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Email: $_userEmail',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            if (_profileImage != null)
+              CircleAvatar(
+                backgroundImage: FileImage(_profileImage!),
+                radius: 50,
+              ),
+            TextButton(
+              onPressed: _pickImage,
+              child: Text("Change Profile Picture"),
             ),
-            SizedBox(height: 20),
-            Text('Username', style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(height: 8),
-            TextFormField(
+            TextField(
               controller: _usernameController,
-              decoration: InputDecoration(
-                hintText: 'Enter Username',
-                border: OutlineInputBorder(),
-              ),
+              decoration: InputDecoration(labelText: "Username"),
             ),
-            SizedBox(height: 20),
-            Text('Bio', style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(height: 8),
-            TextFormField(
+            TextField(
               controller: _bioController,
-              decoration: InputDecoration(
-                hintText: 'Enter a short description about yourself',
-                border: OutlineInputBorder(),
-              ),
+              decoration: InputDecoration(labelText: "Bio"),
             ),
-            SizedBox(height: 20),
-            Text('City', style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(height: 8),
-            TextFormField(
+            TextField(
               controller: _cityController,
-              decoration: InputDecoration(
-                hintText: 'Enter your city',
-                border: OutlineInputBorder(),
-              ),
+              decoration: InputDecoration(labelText: "City"),
             ),
-            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                widget.onUpdate(
-                  _usernameController.text,
-                  _bioController.text,
-                  _cityController.text,
-                );
+              onPressed: () async {
+                await _uploadImage();
                 Navigator.pop(context);
               },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue, // Text color
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12), // Button padding
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8), // Button border radius
-                ),
-                elevation: 3, // Button shadow
-              ),
-              child: Text(
-                'Save',
-                style: TextStyle(fontSize: 16), // Button text style
-              ),
+              child: Text("Save Changes"),
             ),
           ],
         ),
