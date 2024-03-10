c6d617850326dcf7c08261094ce079a1cc28a92c
ridaamirr <ridaamirbashir@gmail.com>
2024-03-10 11:29:31 +0500

    working on search page

--- litxchange/lib/screens/search.dart
+++ litxchange/lib/screens/search.dart
@@ -1,10 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Search Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: 'Search by book name, author, or username',
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: (value) {
+                // Perform search based on the entered text
+                // You can implement search functionality here
+              },
+            ),
+          ),
+          Expanded(
+            child: _buildSearchResults(), // Display search results here
+          ),
+        ],
+      ),
     );
   }
+
+  Widget _buildSearchResults() {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('posts')
+          .where('bookName', isEqualTo: _searchController.text)
+          .snapshots(), // Filter posts by book name
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+        final posts = snapshot.data!.docs;
+        if (posts.isEmpty) {
+          return Center(
+            child: Text('No posts found'),
+          );
+        }
+        return ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            var post = posts[index];
+            var date = post['date'].toDate();
+            var formattedDate = DateFormat.yMMMMd().format(date);
+            String authorName = post['authorName'] ?? 'Unknown Author';
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: Card(
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        formattedDate,
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        '${post['bookName']} by $authorName',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(post['genre']),
+                    ),
+                    post['imageUrl'] != null
+                        ? Image.network(
+                      post['imageUrl'],
+                      fit: BoxFit.cover,
+                    )
+                        : SizedBox.shrink(),
+                    SizedBox(height: 8.0),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            authorName,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.swap_horiz),
+                            onPressed: () {
+                              // Handle more options button press for this post
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
 }
