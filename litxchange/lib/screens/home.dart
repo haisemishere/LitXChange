6f61a4893e05e259c4b7b6d56e853768434037b1
haisemishere <91388126+haisemishere@users.noreply.github.com>
2024-03-26 19:07:35 +0500

    Updated home page

--- litxchange/lib/screens/home.dart
+++ litxchange/lib/screens/home.dart
@@ -12,32 +12,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
-        title: Stack(
-          alignment: Alignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo.png', // Path to your logo image
-              height: 120, // Adjust height as needed
-              width: 200, // Adjust width as needed
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0), // Add padding here
-              child: Text(
-                '',
-                style: TextStyle(
-                  fontSize: 24, // Adjust font size as needed
-                  fontWeight: FontWeight.bold, // Make the text bold
-                  fontFamily: 'Lucida Calligraphy', // Change the font family
-                  color: Colors.black, // Set text color
-                  // Add more decorations as needed
-                ),
-              ),
-            ),
-          ],
+        title: Image.asset(
+          'assets/images/logo.png', // Path to your logo image
+          height: 120, // Adjust height as needed
+          width: 200, // Adjust width as needed
         ),
       ),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('posts')
+        stream: FirebaseFirestore.instance
+            .collection('posts')
             .where('userId', isNotEqualTo: currentUserUid)
             .snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -52,12 +35,13 @@ class HomePage extends StatelessWidget {
             );
           }
           final posts = snapshot.data!.docs;
-          return ListView.builder(
+          return ListView.separated(
             itemCount: posts.length,
+            separatorBuilder: (BuildContext context, int index) => SizedBox(height: 8.0), // Add margin between posts
             itemBuilder: (context, index) {
               var post = posts[index];
               var date = post['date'].toDate();
-              var postId=post['postId'];
+              var postId = post['postId'];
               var formattedDate = DateFormat.yMMMMd().format(date);
               return FutureBuilder(
                 future: _fetchUsername(post['userId']), // Fetch username
@@ -67,69 +51,76 @@ class HomePage extends StatelessWidget {
                     return CircularProgressIndicator();
                   } else if (usernameSnapshot.hasError) {
                     return Text('Error: ${usernameSnapshot.error}');
-                  } else
-                  {
-                    String username = usernameSnapshot.data ?? 'Unknown User';
-                    String authorName = post['authorName'] ?? 'Unknown Author'; // Fetch authorName from the post
-                    return Padding(
-                      padding:
-                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                      child: Card(
-                        elevation: 3,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Text(
-                                formattedDate,
-                                textAlign: TextAlign.right,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
+                  } else {
+                    String username =
+                        usernameSnapshot.data ?? 'Unknown User';
+                    String authorName =
+                        post['authorName'] ?? 'Unknown Author'; // Fetch authorName from the post
+                    String bookCondition =
+                        post['condition'] ?? 'Unknown Condition'; // Fetch book condition from the post
+                    return Container(
+                      width: MediaQuery.of(context).size.width, // Set width to screen width
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor, // Use the same background color as the card
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              formattedDate,
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                            ListTile(
-                              title: Text(
-                                '${post['bookName']} by $authorName', // Display author's name separately
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              '${post['bookName']} by $authorName', // Display author's name separately
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
                               ),
-                              subtitle: Text(post['genre']),
                             ),
-                            post['imageUrl'] != null
-                                ? Image.network(
-                              post['imageUrl'],
-                              fit: BoxFit.cover,
-                            )
-                                : SizedBox.shrink(), // Placeholder for image if not available
-                            SizedBox(height: 8.0),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 16.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    username,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Condition: $bookCondition'),
+                                Text(post['genre']),
+                              ],
+                            ),
+                          ),
+                          post['imageUrl'] != null
+                              ? Image.network(
+                            post['imageUrl'],
+                            fit: BoxFit.cover,
+                          )
+                              : SizedBox.shrink(), // Placeholder for image if not available
+                          SizedBox(height: 8.0),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Row(
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  username,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  IconButton(
-                                    icon: Icon(Icons.swap_horiz),
-                                    onPressed: () {
-                                      sendReq(context,postId);
-                                    },
-                                  ),
-                                ],
-                              ),
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.swap_horiz),
+                                  onPressed: () {
+                                    sendReq(context, postId);
+                                  },
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     );
                   }
@@ -140,10 +131,11 @@ class HomePage extends StatelessWidget {
         },
       ),
     );
-
   }
 
-  Future<String> _fetchUsername(String userId) async {
+
+
+Future<String> _fetchUsername(String userId) async {
     try {
       DocumentSnapshot userData = await FirebaseFirestore.instance
           .collection('users')
@@ -156,10 +148,13 @@ class HomePage extends StatelessWidget {
     }
   }
 
-  void sendReq(BuildContext context, String postId) async {
+  void sendReq(BuildContext context,
+      String postId) async {
     try {
       String userId = FirebaseAuth.instance.currentUser!.uid;
-      String notificationId = FirebaseFirestore.instance.collection('notifications').doc().id; // Generate unique ID for notification
+      String notificationId =
+          FirebaseFirestore.instance.collection('notifications').doc().id;
+      // Generate unique ID for notification
       await FirebaseFirestore.instance.collection('notifications').add({
         'userId': userId,
         'postId': postId,
@@ -201,7 +196,4 @@ class HomePage extends StatelessWidget {
       );
     }
   }
-
-
-
 }
