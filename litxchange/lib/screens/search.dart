cd8fb0e9d9fa16c860c9f8261b7f3730691fc955
haisemishere <91388126+haisemishere@users.noreply.github.com>
2024-04-29 18:56:41 +0500

    fixed search

--- litxchange/lib/screens/search.dart
+++ litxchange/lib/screens/search.dart
@@ -63,12 +63,19 @@ class _SearchPageState extends State<SearchPage> {
     if (searchText.isNotEmpty) {
       if (fieldName == 'userId') {
         searchText = await _fetchUserid(searchText);
+        _searchStream = FirebaseFirestore.instance
+            .collection('posts')
+            .where(fieldName, isEqualTo: searchText)
+            .where('userId', isNotEqualTo: currentUserUid)
+            .snapshots();
       }
+      else{
       _searchStream = FirebaseFirestore.instance
           .collection('posts')
           .where(fieldName, isEqualTo: searchText)
-          .where('userId', isNotEqualTo: currentUserUid)
+          //.where('userId', isNotEqualTo: currentUserUid)
           .snapshots();
+      }
     } else {
       _searchStream = FirebaseFirestore.instance.collection('posts').where('userId', isNotEqualTo: currentUserUid).snapshots();
     }
